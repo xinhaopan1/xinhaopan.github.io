1380a453ec4725e232d402205bbcfbd72a99ffa4
Xhpan <panxinhao98@gmail.com>
2023-03-19 08:27:51 +0100

    Update file.js

--- javascript/file.js
+++ javascript/file.js
@@ -1,12 +1,4 @@
-window.onscroll = function() {fixNavigation()};
-
-var navigation = document.getElementById("navigation");
-var navigationOffset = navigation.offsetTop;
-
-function fixNavigation() {
-  if (window.pageYOffset >= navigationOffset) {
-    navigation.classList.add("fixed");
-  } else {
-    navigation.classList.remove("fixed");
-  }
-}
+window.addEventListener('scroll', function() {
+  var navigation = document.querySelector('.navigation');
+  navigation.classList.toggle('fixed', window.scrollY > 0);
+});
